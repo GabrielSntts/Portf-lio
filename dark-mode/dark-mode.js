a0bd6d788a8d32e52d63022f9b98f2ff99cda991
gabriel <you@example.comgaab.santtos9@gmail.com>
2025-09-21 15:19:13 -0300

    Ultimos ajustes na estilização

--- dark-mode/dark-mode.js
+++ dark-mode/dark-mode.js
@@ -2,18 +2,25 @@ document.addEventListener("DOMContentLoaded", () => {
   const toggleButton = document.getElementById("dark-mode-toggle")
   const body = document.body
   const icon = toggleButton.querySelector("i")
+  const heroVideo = document.querySelector(".hero video")
+
+  // URLs dos vídeos
+  const videoLight = "../img/mp4/programando-js.mp4" 
+  const videoDark = "../img/mp4/video-darkmode.mp4" 
 
   toggleButton.addEventListener("click", () => {
-    // Alterna a classe dark-mode no body
     body.classList.toggle("dark-mode")
 
-    // Troca ícone de Lua para Sol e vice-versa
     if (body.classList.contains("dark-mode")) {
       icon.classList.remove("fa-moon")
       icon.classList.add("fa-sun")
+      heroVideo.src = videoDark // muda vídeo para modo escuro
     } else {
       icon.classList.remove("fa-sun")
       icon.classList.add("fa-moon")
+      heroVideo.src = videoLight
     }
+
+    heroVideo.play()
   })
 })
